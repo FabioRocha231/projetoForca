2f19c3e6698cec52e70e7a4d12096cff6a1cff14
FabioRocha231 <fabioharoldo1@gmail.com>
2021-08-24 09:27:45 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,210 @@
+/*   Elemento HTML referente a categoria
+categoria = document.querySelector("#category");
+   Elemento HTML referente a lista das letras erradas
+letrasErradas = document.querySelector(".wrongLetters");
+   Elemento HTML referente a palavra oculta
+   Utilizaremos esse mesmo elemento para exibir as mensagens do jogo
+palavraInterface = document.querySelector(".dashes");
+   Array com elementos HTML referentes aos olhos do personagem 
+olhos = Array.from(document.querySelectorAll(".eyes"));
+   Array com elementos HTML referentes as partes do corpo 
+let partesBoneco = Array.from(document.querySelectorAll("#person div"));
+partesBoneco = partesBoneco.slice(2, partesBoneco.length);
+   Palavra corrente
+palavraProposta;
+   Lista das letras erradas 
+letrasErradasArray = [];
+   Index da parte do corpo corrente
+indiceBoneco;
+   Numero de chances do jogador 
+numTentativas = 7;
+   Valor para opacidade dos olhos 
+opacidadeOlhos = 0.3;*/
+
+let partesBoneco = Array.from(document.querySelectorAll("#person div"));
+partesBoneco = partesBoneco.slice(2, partesBoneco.length);
+
+const engrenagem = {
+  categoria: document.querySelector("#category"),
+  letrasErradas: document.querySelector(".wrongLetters"),
+  palavraInterface: document.querySelector(".dashes"),
+  olhos: Array.from(document.querySelectorAll(".eyes")),
+  palavraProposta: "",
+  letrasErradasArray: [],
+  indiceBoneco: "",
+  numTentativas: 7,
+  opacidadeOlhos: 0.3,
+};
+
+let {
+  categoria,
+  letrasErradas,
+  palavraInterface,
+  olhos,
+  palavraProposta,
+  letrasErradasArray,
+  indiceBoneco,
+  numTentativas,
+  opacidadeOlhos,
+} = engrenagem;
+
+const categorias = {
+  frutas: [
+    "banana",
+    "maça",
+    "laranja",
+    "mamao",
+    "uva",
+    "melancia",
+    "melao",
+    "pera",
+    "limao",
+  ],
+  profissoes: [
+    "engenheiro",
+    "advogado",
+    "medico",
+    "professor",
+    "pescador",
+    "sushiman",
+    "fisico",
+  ],
+  animais: [
+    "papagaio",
+    "galo",
+    "cachorro",
+    "gato",
+    "galinha",
+    "cavalo",
+    "lhama",
+    "avestruz",
+  ],
+  cores: ["amarelo", "azul", "laranja", "roxo", "vermelho", "marrom"],
+};
+
+function retornaArrayCategorias() {
+  return Object.keys(categorias);
+}
+
+function retornaCategoria() {
+  const arrayCategorias = retornaArrayCategorias();
+  let indiceCategoria = Math.floor(Math.random() * arrayCategorias.length);
+  return arrayCategorias[indiceCategoria];
+}
+
+function exibeCategoria() {
+  categoria.innerHTML = retornaCategoria();
+}
+
+function retornaNumAleatorio(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function definePalavraProposta() {
+  const arrayPalavras = categorias[categoria.innerHTML];
+  let indicePalavra = Math.floor(Math.random() * arrayPalavras.length);
+  palavraProposta = arrayPalavras[indicePalavra];
+  console.log(palavraProposta);
+  ocultaPalavra();
+}
+
+function ocultaPalavra() {
+  let palavraOcultada = "";
+  for (let i = 0; i < palavraProposta.length; i++) {
+    palavraOcultada += "-";
+  }
+  exibePalavraInterface(palavraOcultada);
+}
+function exibePalavraInterface(palavra) {
+  palavraInterface.innerHTML = palavra;
+}
+
+function tentativa(letra) {
+  if (palavraProposta.includes(letra)) {
+    atualizaPalavraInterface(letra);
+  } else {
+    letrasErradasArray.push(letra);
+    letrasErradas.innerHTML = `letras erradas: ${letrasErradasArray}`;
+    if (partesBoneco.length > indiceBoneco) {
+      desenhaBoneco();
+    }
+  }
+  verificaFimDeJogo();
+}
+
+function verificaFimDeJogo() {
+  if (!palavraInterface.innerHTML.includes("-")) {
+    exibePalavraInterface("Voce Venceu!!!");
+    window.removeEventListener("keypress", retornaLetra);
+  } else if (letrasErradasArray.length >= numTentativas) {
+    desenhaOlhos();
+    exibePalavraInterface("voce perdeu!!!");
+    window.removeEventListener("keypress", retornaLetra);
+  }
+}
+
+function atualizaPalavraInterface(letra) {
+  let palavraAux = "";
+  for (let i = 0; i < palavraProposta.length; i++) {
+    if (palavraProposta[i] === letra) {
+      palavraAux += letra;
+    } else if (palavraInterface.innerHTML[i] != "-") {
+      palavraAux += palavraInterface.innerHTML[i];
+    } else {
+      palavraAux += "-";
+    }
+  }
+  exibePalavraInterface(palavraAux);
+}
+
+/*
+Recebe o evento do teclado e passa apenas o valor da letra para a função tentativa
+*/
+function retornaLetra(e) {
+  tentativa(e.key);
+}
+
+/*
+Desenha a parte do corpo corrente
+*/
+function desenhaBoneco() {
+  partesBoneco[indiceBoneco].classList.remove("hide");
+  indiceBoneco++;
+}
+
+/* 
+Desenha os olhos do personagem
+*/
+function desenhaOlhos() {
+  olhos.forEach((olho) => {
+    olho.style.opacity = 1;
+    olho.style.zIndex = 10;
+  });
+}
+
+/*
+Oculta as partes do corpo do personagem
+*/
+function ocultaBoneco() {
+  olhos.forEach((olho) => {
+    olho.style.opacity = opacidadeOlhos;
+  });
+  partesBoneco.forEach((parteBoneco) => {
+    parteBoneco.classList.add("hide");
+  });
+}
+
+/*
+Inicia as configurações do jogo
+*/
+function iniciaJogo() {
+  indiceBoneco = 0;
+  letrasErradasArray = [];
+  desenhaBoneco();
+  exibeCategoria();
+  definePalavraProposta();
+  letrasErradas.innerHTML = "Letras erradas: ";
+  window.addEventListener("keypress", retornaLetra);
+}
+
+window.addEventListener("load", iniciaJogo);
